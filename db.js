4743268b4ea381c8d828b88f5aa0e6ad76ed3780
Satyam <karnsaty69@gmail.com>
2025-03-26 10:54:41 +0545

    modified .env and db.js

--- db.js
+++ db.js
@@ -8,10 +8,10 @@ let db_1;
 
 const connectDB = async () => {
     try {
-        // const client = new MongoClient(`mongodb+srv://${userName}:${password}@cluster0.hfsa6.mongodb.net/${dbName}?retryWrites=true&w=majority&appName=Cluster0`
-        // ); +
+        const client = new MongoClient(`mongodb+srv://${userName}:${password}@cluster0.hfsa6.mongodb.net/${dbName}?retryWrites=true&w=majority&appName=Cluster0`
+        ); 
 
-        const client=new MongoClient('mongodb://localhost:27017');
+        // const client=new MongoClient('mongodb://localhost:27017');
 
         await client.connect();
         console.log('Connected !!! 🌍🌎🌏');
